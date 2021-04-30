110d7f06d8dbba446ec44c953e015db9eeed2710
Corey White <smortopahri@gmail.com>
2021-04-30 15:48:59 -0400

    Added infer_steady function

--- pyfcm/analysis/tools.py
+++ pyfcm/analysis/tools.py
@@ -7,8 +7,25 @@ Created on Fri Apr 30 13:40:00 2021
          ctwhite@ncsu.edu
 """
 
+from enum import Enum
 import numpy as np
 import math
+
+
+class SquashingFucntion(Enum):
+    SIG = "sig"
+    TANH = "tanh"
+    BIV = "biv"
+    TRIV = "triv"
+
+
+class InferenceRule(Enum):
+    K = "k"
+    MK = "mk"
+    R = "r"
+
+
+# 'SIG' in SquashingFucntion.__members__
 
 
 def _transform(x, n, f_type, landa):
@@ -61,3 +78,55 @@ def _transform(x, n, f_type, landa):
             else:
                 x_new[i] = -1
         return x_new
+
+
+def infer_steady(init_vec, adjmatrix, n, landa, f_type="sig", infer_rule="mk"):
+    """
+     Every concept in the FCM graph has a value Ai that expresses the quantity of its
+     corresponding physical value and it is derived by the transformation of the fuzzy values
+     assigned by who developed the FCM to numerical values. The value Ai of each concept Ci is
+     calculated during each simulation step, computing the influence of other concepts to the
+     specific concept by selecting one of the following equations (inference rules).
+
+     k = Kasko
+     mk = Modified Kasko
+     r = Rescaled Kasko
+
+    Parameters
+       ----------
+       init_vec : numpy.ndarray
+           Inital activation vector.
+       adjmatrix : numpy.ndarray
+           Adjacency matrix of the fuzzy congintive model.
+       n : int
+           The number of concepts in the adjacency matrix.
+       landa : int
+           The lambda threshold value used in the squashing fuciton between 0 - 10.
+       f_type : str (optional)
+           Sigmoid = "sig", Hyperbolic Tangent = "tanh", Bivalent = "biv", Trivalent = "triv"
+       infer_rule : str (optional)
+           Kasko = "k", Modified Kasko = "mk", Rescaled Kasko = "r" :Default: "mk"
+
+       Returns
+           -------
+           Activation Vector : numpy.ndarray
+    """
+    act_vec_old = init_vec
+    resid = 1
+    while resid > 0.00001:
+        act_vec_new = np.zeros(n)
+        x = np.zeros(n)
+        if infer_rule == "k":
+            x = np.matmul(adjmatrix, act_vec_old)
+        if infer_rule == "mk":
+            x = act_vec_old + np.matmul(adjmatrix, act_vec_old)
+        if infer_rule == "r":
+            x = (2 * act_vec_old - np.ones(n)) + np.matmul(
+                adjmatrix, (2 * act_vec_old - np.ones(n))
+            )
+
+        act_vec_new = _transform(x, n, f_type, landa)
+        resid = max(abs(act_vec_new - act_vec_old))
+        act_vec_old = act_vec_new
+
+    return act_vec_new
